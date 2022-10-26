7d0598745aa0db5155ab1f0a06aa7c79f875e307
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2022-10-26 00:27:16 -0400

    Initial Instance - Not Tested

--- main.tf
+++ main.tf
@@ -30,3 +30,23 @@ resource "google_compute_network" "vpc_network" {
   name                    = "infra"
   auto_create_subnetworks = false
 }
+
+#Create Compute Instance
+resource "google_compute_instance" "vm_instance"{
+  name          = "terraform-instance"
+  machine_type  = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    #A default network created for GCP projects
+    network = "${google_compute_network.vpc_network.self_link}"
+    access_config {
+    }
+  }
+
+}
